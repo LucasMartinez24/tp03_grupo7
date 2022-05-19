2333e9ecde44e320b3fef795221e8af00b0cdfad
LucasMartinez24 <martinezlucas1478@gmail.com>
2022-05-18 23:37:40 -0300

    first commit

---
+++ src/main/java/ar/edu/unju/fi/tp03_grupo7/Tp03Grupo7Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp03_grupo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp03Grupo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp03Grupo7Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/fi/tp03_grupo7/Tp03Grupo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp03_grupo7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp03Grupo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
