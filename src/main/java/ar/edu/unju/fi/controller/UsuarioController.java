1308d2997e30d40443e68462fc7bb129c883d483
lucacu <lucas.16.llado@gmail.com>
2022-05-27 16:19:14 -0300

    cambios varios

--- src/main/java/ar/edu/unju/fi/controller/UsuarioController.java
+++ src/main/java/ar/edu/unju/fi/controller/UsuarioController.java
@@ -1,9 +1,13 @@
 package ar.edu.unju.fi.controller;
 
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,6 +16,8 @@ import ar.edu.unju.fi.until.Lista;
 
 @Controller
 public class UsuarioController {
+  private static final Log LUCAS=LogFactory.getLog(UsuarioController.class);
+
   @Autowired
   Persona listaPersona;
   @Autowired
@@ -22,6 +28,7 @@ public class UsuarioController {
     ModelAndView vista= new ModelAndView();
     vista.addObject("formulario");
     vista.addObject("usuario", listaPersona);
+    vista.addObject("band","false");
     return vista;
   }
   @PostMapping("/formulario")
@@ -29,5 +36,25 @@ public class UsuarioController {
     list.getLista().add(user);
     System.out.println("Tamaño Lista: " + list.getLista().size());
     return "redirect:/formulario";
+  }
+  @GetMapping("/lista")
+  public ModelAndView getlista(){
+    ModelAndView vista = new ModelAndView("Listado");
+    vista.addObject("listaUsuario", list.getLista());
+    return vista;
+  }
+  @GetMapping("/editarUsuario/{dni}")
+  public ModelAndView edituser(@PathVariable(name="dni") Long dni){
+    Persona usuarioencontrado = new Persona();
+    for(int i=0;i<list.getLista().size();i++){
+      if(list.getLista().get(i).getDni().equals(dni)){
+        usuarioencontrado = list.getLista().get(i);
+      }
+    }
+    LUCAS.fatal("Error de entrada"+usuarioencontrado.getDni());
+    ModelAndView encontrado = new ModelAndView("formulario");
+    encontrado.addObject("usuario", usuarioencontrado);
+    encontrado.addObject("band","true");
+    return encontrado;
   }
 }
--- src/main/java/ar/edu/unju/fi/model/persona.java
+++ src/main/java/ar/edu/unju/fi/model/persona.java
@@ -5,13 +5,29 @@ import org.springframework.stereotype.Component;
 @Component
 public class Persona {
   private String usuario;
+  private String email;
   private String contraseña;
+  private Long dni;
   public Persona(){
     
   }
-  public Persona(String usuario, String contraseña) {
+  public Long getDni() {
+    return dni;
+  }
+  public void setDni(Long dni) {
+    this.dni = dni;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public Persona(String usuario, String contraseña, String email, Long dni) {
     this.usuario = usuario;
     this.contraseña = contraseña;
+    this.email = email;
+    this.dni = dni;
   }
   public String getUsuario() {
     return usuario;
